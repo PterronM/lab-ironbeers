862c93b0688dfc8764da11c82f250fa65d173bb3
Pilar Terron <pilarterronmartin@gmail.com>
2023-02-08 18:46:18 +0100

    Completado + Bonus

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+hbs.registerPartials(__dirname + "/views/partials")
+
 // Register the location for handlebars partials here:
 
 // ...
@@ -19,7 +21,58 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
+  
   res.render('index');
 });
+
+app.get('/beers', (req, res) => {
+
+  punkAPI.getBeers()
+  .then(beersFromApi => {
+    console.log('Beers from the database: ', beersFromApi)
+
+    res.render("beers",{
+      beersFromApi: beersFromApi
+    });
+  })
+  .catch(error => console.log(error));
+
+
+});
+
+app.get('/beers/:beerId', async(req, res) => {
+
+  try{
+    
+    const response = await punkAPI.getBeer(req.params.beerId)
+    
+    
+    res.render("beerClick",{
+      beersFromApi: response
+
+  })
+
+  }catch(error){
+    console.log(error)
+  }
+
+
+});
+
+app.get('/random-beer', (req, res) => {
+
+  punkAPI.getRandom()
+  .then(responseFromAPI => {
+        
+    res.render("random-beer",{
+      responseFromAPI: responseFromAPI
+    });
+  })
+  .catch(error => console.log(error));
+
+
+});
+
+
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
